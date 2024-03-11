4f00691f85faa75c59fc653997a3a18b866bb0e5
abdsymoh <abdsydev@gmail.com>
2024-03-11 22:10:01 +0200

    add setting menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,17 @@ btnUP.onclick = function () {
     behavior: "smooth",
   });
 };
+
+// Settings Toggle
+let settingsToggle = document.getElementById("settingsToggle");
+settingsToggle.onclick = function () {
+  if (settingsToggle.querySelector("i").classList.contains("fa-circle-xmark")) {
+    settingsToggle.querySelector("i").classList.add("fa-gear");
+    settingsToggle.querySelector("i").classList.remove("fa-circle-xmark");
+    document.querySelector(".settings-menu").style = ` top: -99px`;
+  } else {
+    settingsToggle.querySelector("i").classList.add("fa-circle-xmark");
+    settingsToggle.querySelector("i").classList.remove("fa-gear");
+    document.querySelector(".settings-menu").style = ` top: 0`;
+  }
+};
